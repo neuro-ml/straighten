2f450d9b6271a035586f2deff8cf5a9ab1c9833d
Max <maxs987@gmail.com>
2021-08-07 00:19:40 +0300

    fixed an import issue

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-from straighten import __version__
-
 classifiers = '''Development Status :: 4 - Beta
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
@@ -14,7 +12,7 @@ with open('requirements.txt', encoding='utf-8') as file:
 setup(
     name='straighten',
     packages=find_packages(include=('straighten',)),
-    version=__version__,
+    version='0.2.0',
     descriprion='Interpolation along multidimensional curves.',
     license='MIT',
     keywords=[],
